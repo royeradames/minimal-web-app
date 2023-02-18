75494b8e80838b2ae17a20e2e277d95b4a620e21
Royer Adames <royeraadames@gmail.com>
2023-02-18 18:36:13 -0500

    all images request make then cacheFirst

--- service-worker.js
+++ service-worker.js
@@ -3,4 +3,9 @@
 // import workbox from a sdn
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/6.0.2/workbox-sw.js")
 
-
+workbox.routing.registerRoute(
+  //catch all images
+  ({request}) => request.destination === 'image',
+  //  decide on catch strategy
+  new workbox.strategies.CacheFirst()
+)
